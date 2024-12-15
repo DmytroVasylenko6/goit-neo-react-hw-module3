b6cfda2cc5ce962f498dd3513f6d12bb759bdc99
DmytroVasylenko6 <tredstoun651@ukr.net>
2024-12-15 23:01:57 +0200

    save to localStorage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from 'react'
+import { useState, ChangeEvent, useEffect } from 'react'
 import Section from './Section'
 import ContactsList from './ContactsList'
 import ContactForm from './ContactForm'
@@ -24,10 +24,18 @@ const initialContacts = [
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }
 ]
+const getInitialContacts = () => {
+  const savedContacts = localStorage.getItem('contacts')
+  return savedContacts ? JSON.parse(savedContacts) : initialContacts
+}
 
 const App = () => {
-  const [contacts, setContacts] = useState<Contact[]>(initialContacts)
+  const [contacts, setContacts] = useState<Contact[]>(getInitialContacts)
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts])
 
   const deleteContact = (contactId: string): void => {
     setContacts(contacts.filter((contact) => contact.id !== contactId))
